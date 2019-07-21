ed6893dd4dd75b351592a7131c4a4531998c1e85
moraruuvasile <vaseoa@yahoo.com>
2019-07-21 18:52:57 +0100

    1-implement clickablePost.setOnItemClickListiner()

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -41,5 +43,18 @@ public class MainActivity extends AppCompatActivity {
 		clickablePosts = findViewById(R.id.appsListView);
 		ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,posts);
 		clickablePosts.setAdapter(arrayAdapter);
+
+		clickablePosts.setOnItemClickListener(
+				new AdapterView.OnItemClickListener() {
+					@Override
+					public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+						ffsdfsdf
+								ffsdfsdfsdfsd
+								dfsfsd
+										dsfsd
+
+					}
+				}
+		);
 	}
 }
